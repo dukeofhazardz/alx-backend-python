b631fce32dada764a9f92dce54afe93d6485776a
Daniel John <dj71286@gmail.com>
2023-08-07 21:09:28 +0100

    Measure the runtime

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -2,13 +2,14 @@
 
 """Asynchronous functions"""
 
+from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """ A function that return the list of all the delays (float values)
         in ascending order without using sort() because of concurrency. """
-    delay_list: list[float] = []
+    delay_list: List[float] = []
 
     for i in range(n):
         delay: float = await wait_random(max_delay)
--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -3,14 +3,15 @@
 """Asynchronous functions"""
 
 import time
+import asyncio
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-async def measure_time(n: int, max_delay: int) -> float:
+def measure_time(n: int, max_delay: int) -> float:
     """ A function that measures the total execution time for
         wait_n(n, max_delay), and returns total_time / n """
     startTime: float = time.perf_counter()
-    wait_n(n, max_delay)
+    asyncio.run(wait_n(n, max_delay))
     endTime: float = time.perf_counter()
     totalTime: float = endTime - startTime
     return totalTime / n
