ca26a8d9a8c027dd39ccaaa4da78c22fc5398aaf
Daniel John <dj71286@gmail.com>
2023-08-07 20:42:37 +0100

    Fixed lines

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -4,6 +4,7 @@
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
+
 async def wait_n(n: int, max_delay: int) -> list:
     """ A function that  return the list of all the delays (float values)
         in ascending order without using sort() because of concurrency. """
